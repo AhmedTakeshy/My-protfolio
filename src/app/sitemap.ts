8e3f4dfa7ddf6df22fb91de55042d21de0351f3a
Ahmed Abdelsamie <37537011+AhmedTakeshy@users.noreply.github.com>
2023-10-22 13:34:40 +0300

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://takeshy/works',
+      url: 'https://takeshy.works',
       lastModified: new Date().toISOString(),
     },
   ]
