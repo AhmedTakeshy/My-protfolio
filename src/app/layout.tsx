aa1631f99551b0c59159f123e145beed7f1e1b33
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2023-12-13 01:19:55 +0300

    Refactor font imports and update metadata in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Orbitron,Rajdhani,Aldrich } from "next/font/google";
+import { Orbitron, Rajdhani, Aldrich } from "next/font/google";
 
-const orbitron = Orbitron({ subsets: ["latin"], weight: ["400", "900"], display: "swap",variable:"--font-orbitron" });
+const orbitron = Orbitron({ subsets: ["latin"], weight: ["400", "900"], display: "swap", variable: "--font-orbitron" });
 const rajdhani = Rajdhani({ subsets: ["latin"], weight: ["300", "400", "500", "600", "700"], display: "swap", variable: "--font-rajdhani" });
-const aldrich = Aldrich({ subsets: ["latin"], weight: "400", display: "swap",variable:"--font-aldrich" });
+const aldrich = Aldrich({ subsets: ["latin"], weight: "400", display: "swap", variable: "--font-aldrich" });
 
 
 export const metadata: Metadata = {
   title: "Ahmed Takeshy | Frontend Developer",
   description: "Frontend developer based in Turkey. I create responsive websites that are displayed on all devices desktops and smartphones. I am familiar with HTML5, CSS3, JavaScript, React, NextJS, and more.",
   keywords: "Web Developer, Frontend Developer, React Developer, NextJS Developer, JavaScript Developer, HTML Developer, CSS Developer, Ahmed Takeshy, Ahmed, Takeshy, Ahmed Takeshy Portfolio, Ahmed Takeshy Website, Ahmed Takeshy Frontend Developer, Ahmed Takeshy Web Developer, Ahmed Takeshy Frontend, Ahmed Takeshy Web, Ahmed Takeshy",
-  openGraph:{
+  openGraph: {
     type: "website",
     locale: "en_US",
     url: "https://takeshy.works",
@@ -69,7 +69,16 @@ export const metadata: Metadata = {
       url: "/icons/android-chrome-512x512.png",
     },
   ],
-  applicationName: "Takeshy",
+  applicationName: "Ahmed Takeshy",
+  generator: "Ahmed Takeshy",
+  referrer: 'origin-when-cross-origin',
+  authors: {name: "Ahmed Takeshy", url: "https://takeshy.works"},
+  creator: "Ahmed Takeshy",
+  publisher: "Ahmed Takeshy",
+  metadataBase: new URL('https://takeshy.works'),
+  alternates: {
+    canonical: '/',
+  },
   twitter: {
     site: "https://takeshy.works",
     card: "summary_large_image",
@@ -79,14 +88,13 @@ export const metadata: Metadata = {
     title: "Takeshy",
     description: "Frontend developer based in Turkey. I create responsive websites that are displayed on all devices desktops and smartphones. I am familiar with HTML5, CSS3, JavaScript, React, NextJS, and more.",
     images: "/images/naviro.png",
-  },creator: "Developed by Ahmed Takeshy",
+  },
   robots: "index, follow",
   classification: `
     web development and design, web development, web design, web developer, web designer, web development company, web design company, web development agency, web design agency, web development services, web design services, web development firm, web design firm, web development studio, web design studio, web development company near me, web design company near me, web development agency near me, web design agency near me, web development services near me, web design services near me, web development firm near me, web design firm near me, web development studio near me, web design studio near me, web development company in turkey, web design company in turkey, web development agency in turkey, web design agency in turkey, web development services in turkey, web design services in turkey, web development firm in turkey, web design firm in turkey, web development studio in turkey, web design studio in turkey
 ` ,
   category: "web development and design, web development, web design, web developer, web designer, web development company, web design company, web development agency, web design agency, web development services, web design services, web development firm, web design firm, web development studio, web design studio, web development company near me, web design company near me, web development agency near me, web design agency near me, web development services near me, web design services near me, web development firm near me, web design firm near me, web development studio near me, web design studio near me, web development company in turkey, web design company in turkey, web development agency in turkey, web design agency in turkey, web development services in turkey, web design services in turkey, web development firm in turkey, web design firm in turkey, web development studio in turkey, web design studio in turkey",
-  metadataBase: new URL("https://takeshy.works"),
-  other:{
+  other: {
     copyRight: "© 2023 Ahmed Takeshy. All rights reserved.",
     rating: "general",
     Distribution: "global",
@@ -100,21 +108,23 @@ export const metadata: Metadata = {
     ogUrl: "https://takeshy.works",
     ogTitle: "Takeshy",
     ogDescription: "Frontend developer based in Turkey. I create responsive websites that are displayed on all devices desktops and smartphones. I am familiar with HTML5, CSS3, JavaScript, React, NextJS, and more.",
-    
-  }
+  },
+  appLinks: {
+    web: { url: "https://takeshy.works" },
+  },
 };
 
 
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <html lang="en">
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
       <body className={`${orbitron.variable} ${rajdhani.variable} ${aldrich.variable}`}>
         {children}
       </body>
-      </html>
-    )
-  }
+    </html>
+  )
+}
