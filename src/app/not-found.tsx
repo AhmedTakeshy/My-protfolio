c09b4d79811c8cda7dc0e759df5c7ba710d6f593
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2023-12-13 02:30:49 +0300

    Add not-found page component

---
+++ src/app/not-found.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export default function NotFound() {
+    return (
+        <div className="flex flex-col items-center justify-between w-screen h-screen px-5 lg:flex-row bg-site">
+            <div className="w-full mx-8 text-left lg:w-1/2">
+                <span className="mb-6 font-bold text-7xl text-accent">404</span>
+                <p className="mb-12 text-2xl font-light leading-normal text-white md:text-3xl">
+                    Sorry but it seems you are lost in my app, <br />
+                    but don't worry, I'll help you find your way back.
+                </p>
+                <Link href="/" className="inline px-5 py-3 text-sm font-medium leading-5 text-white shadow-2xl focus:outline-none btn">
+                    Here's your way back
+                </Link>
+            </div>
+            <div className="w-full mx-5 my-12 lg:flex lg:w-1/2">
+                <Image width={700} height={436} src="/img/not-found.png" className="w-full" alt="Page not found" />
+            </div>
+        </div>
+    )
+}
