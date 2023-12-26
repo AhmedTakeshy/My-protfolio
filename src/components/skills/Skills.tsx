c724891f63e26558eb08c24d811b138bc034ef25
Ahmed Takeshy <ahmedtakeshy7@gmail.com>
2023-12-26 09:52:44 +0300

    Update react-multi-carousel import in Skills component

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,10 +1,11 @@
+"use client"
 import React from "react";
-import Carousel from "react-multi-carousel";
+import Carousel,{ResponsiveType} from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import Skill from "./Skill";
 
 export default function Skills () {
-  const responsive = {
+  const responsive:ResponsiveType = {
     superLargeDesktop: {
       breakpoint: { max: 4000, min: 3000 },
       items: 5,
