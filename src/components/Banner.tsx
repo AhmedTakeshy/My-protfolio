a0ab60083ce82f2fb72c0771dcf127ee1646115f
Ahmed Takeshy <ahmedtakeshy7@gmail.com>
2023-12-26 09:53:12 +0300

    Refactor Banner component imports and animations

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,10 +1,10 @@
 "use client"
 import Image from "next/image";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { Link } from "react-scroll";
+import Link from "next/link";
 import { TypeAnimation } from "react-type-animation";
-import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
+import { MotionDiv, MotionH1, MotionP } from "../motionsDev";
 
 const Banner = () => (
   <section
@@ -14,7 +14,7 @@ const Banner = () => (
     <div className="container mx-auto">
       <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
         <div className="flex-1 text-center font-secondary lg:text-left">
-          <motion.h1
+          <MotionH1
             variants={fadeIn("up", 0.3)}
             initial="hidden"
             whileInView={"show"}
@@ -22,8 +22,8 @@ const Banner = () => (
             className="text-[55px] font-bold leading-[0.8] lg:text-[110px] pointer-events-none"
           >
             AHMED <span>TAKESHY</span>
-          </motion.h1>
-          <motion.div
+          </MotionH1>
+          <MotionDiv
             variants={fadeIn("up", 0.4)}
             initial="hidden"
             whileInView={"show"}
@@ -38,8 +38,8 @@ const Banner = () => (
               className="text-accent"
               repeat={Infinity}
             />
-          </motion.div>
-          <motion.p
+          </MotionDiv>
+          <MotionP
             variants={fadeIn("up", 0.5)}
             initial="hidden"
             whileInView={"show"}
@@ -50,52 +50,52 @@ const Banner = () => (
             with a strong focus on attention to detail and problem-solving.
             Skilled in both independent and team settings, committed to website
             development and continuous improvement.
-          </motion.p>
-          <motion.div
+          </MotionP>
+          <MotionDiv
             variants={fadeIn("up", 0.6)}
             initial="hidden"
             whileInView={"show"}
             viewport={{ once: false, amount: 0.7 }}
             className="flex items-center mx-auto mb-12 max-w-max gap-x-6 lg:mb-8 lg:mx-0"
           >
-            <Link to="contact" smooth={true}>
+            <Link href="/#contact">
               <button className="btn btn-lg">Contact me</button>
             </Link>
-            <a className="text-gradient btn-link" href="/Ahmed.pdf" download target="_blank" rel="noopener noreferrer">
+            <Link className="text-gradient btn-link" href="/Ahmed.pdf" download target="_blank" rel="noopener noreferrer">
               My Resume
-            </a>
-          </motion.div>
-          <motion.div
+            </Link>
+          </MotionDiv>
+          <MotionDiv
             variants={fadeIn("up", 0.7)}
             initial="hidden"
             whileInView={"show"}
             viewport={{ once: false, amount: 0.7 }}
             className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0 "
           >
-            <a
+            <Link
               href="https://github.com/AhmedTakeshy"
               target={"_blank"}
               rel="noreferrer"
             >
               <FaGithub />
-            </a>
-            <a
+            </Link>
+            <Link
               href="https://www.linkedin.com/in/ahmedabdelsamiee/"
               target={"_blank"}
               rel="noreferrer"
             >
               <FaLinkedin />
-            </a>
-          </motion.div>
+            </Link>
+          </MotionDiv>
         </div>
-        <motion.div
+        <MotionDiv
           variants={fadeIn("down", 0.5)}
           initial="hidden"
           whileInView={"show"}
           className="hidden lg:flex flex-1 max-w-[320px] lg:mx-w-[482]"
         >
           <Image width={300} height={300} src="/img/header-img.svg" alt="header img" className="img" />
-        </motion.div>
+        </MotionDiv>
       </div>
     </div>
   </section>
