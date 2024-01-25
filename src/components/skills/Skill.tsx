eb33809aa3e8a6c46d716f835786b75c939d566e
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-01-25 03:16:15 +0300

    Refactor Skill component props formatting

--- src/components/skills/Skill.tsx
+++ src/components/skills/Skill.tsx
@@ -1,19 +1,20 @@
 import Image from "next/image";
-type Props= {
+
+type Props = {
   src: string;
   alt: string;
   name: string;
 }
 
-export default function Skill ({src,alt,name}: Props) {
+export default function Skill({ src, alt, name }: Props) {
   return (
     <div className="flex flex-col items-center ">
-      <Image 
-      width={200}
-      height={200}
-      src={src} 
-      alt={alt} 
-      className="w-[45%] mb-6" />
+      <Image
+        width={200}
+        height={200}
+        src={src}
+        alt={alt}
+        className="w-[45%] mb-6" />
       <h5 className="font-semibold tracking-widest pointer-events-none">
         {name}
       </h5>
