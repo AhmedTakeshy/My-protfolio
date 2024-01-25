d1dcbbacf887d702f7f1887cfc8e101f60d18257
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-01-25 03:06:19 +0300

    Update sitemap URL in sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,9 @@
 import { MetadataRoute } from 'next'
- 
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://takeshy.works',
+      url: 'https://takeshy.tech/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
