73879c53133fb31fdf8a40c60d6e897ab9aac45f
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-01-25 03:05:53 +0300

    Refactor import statements in NotFound component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image"
 import Link from "next/link"
+import Image from "next/image"
 
 export default function NotFound() {
     return (
@@ -8,10 +8,10 @@ export default function NotFound() {
                 <span className="mb-6 font-bold text-7xl text-accent">404</span>
                 <p className="mb-12 text-2xl font-light leading-normal text-white md:text-3xl">
                     Sorry but it seems you are lost in my app, <br />
-                    but don't worry, I'll help you find your way back.
+                    but don&apos;t worry, I&apos;ll help you find your way back.
                 </p>
                 <Link href="/" className="inline px-5 py-3 text-sm font-medium leading-5 text-white shadow-2xl focus:outline-none btn">
-                    Here's your way back
+                    Here&apos;s your way back
                 </Link>
             </div>
             <div className="w-full mx-5 my-12 lg:flex lg:w-1/2">
