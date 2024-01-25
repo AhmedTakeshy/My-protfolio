db521e48aa33b267ed0ecc220b8a0528b6e19ba2
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-01-25 03:16:11 +0300

    Update Nav component icons and add accessibility attributes

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,9 @@
 "use client"
+"use client"
 import { BiHomeAlt, BiUser, BiCode } from "react-icons/bi";
 import { BsBriefcase, BsChatSquare } from "react-icons/bs";
 import { Link } from "react-scroll";
-export default function Nav () {
-
+export default function Nav() {
   return (
     <nav className="fixed z-50 w-full overflow-hidden bottom-3 lg:bottom-8">
       <div className="container mx-auto">
@@ -17,46 +17,75 @@ export default function Nav () {
             smooth={true}
             offset={-200}
             spy={true}
-            className={`cursor-pointer w-[60px] h-[60px] flex items-center justify-center`}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+            aria-label="home"
+            aria-description="link to home section"
+            title="link to home section"
           >
-            <BiHomeAlt />
+            <BiHomeAlt
+              aria-label="icon"
+              aria-description="home icon"
+            />
           </Link>
           <Link
             to="skills"
             activeClass="active"
             smooth={true}
             spy={true}
-            className={`cursor-pointer w-[60px] h-[60px] flex items-center justify-center`}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+            aria-label="skills"
+            aria-description="link to skills section"
+            title="link to skills section"
           >
-            <BiCode />
+            <BiCode
+              aria-label="icon"
+              aria-description="code icon"
+            />
           </Link>
           <Link
             to="work"
             activeClass="active"
             smooth={true}
             spy={true}
-            className={`cursor-pointer w-[60px] h-[60px] flex items-center justify-center`}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+            aria-label="work"
+            aria-description="link to work section"
+            title="link to work section"
           >
-            <BsBriefcase />
+            <BsBriefcase
+              aria-label="icon"
+              aria-description="briefcase icon"
+            />
           </Link>
           <Link
             to="about"
             activeClass="active"
             smooth={true}
             spy={true}
-            className={`cursor-pointer w-[60px] h-[60px] flex items-center justify-center`}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+            aria-label="about"
+            aria-description="link to about section"
+            title="link to about section"
           >
-            <BiUser />
+            <BiUser
+              aria-label="icon"
+              aria-description="user icon"
+            />
           </Link>
           <Link
             to="contact"
             activeClass="active"
             smooth={true}
             spy={true}
-            offset={50}
-            className={`cursor-pointer w-[60px] h-[60px] flex items-center justify-center`}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+            aria-label="contact"
+            aria-description="link to contact section"
+            title="link to contact section"
           >
-            <BsChatSquare />
+            <BsChatSquare
+              aria-label="icon"
+              aria-description="chat icon"
+            />
           </Link>
         </div>
       </div>
