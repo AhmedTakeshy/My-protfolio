f9350a05bed722ffe3825c7d0daeb977cffbd97d
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-01-25 03:05:33 +0300

    Add fadeIn animation variants

--- src/variants.ts
+++ src/variants.ts
@@ -1,4 +1,6 @@
-export const fadeIn = (direction:string, delay:number) => {
+type Direction = "up" | "down" | "left" | "right";
+
+export const fadeIn = (direction: Direction, delay: number) => {
   return {
     hidden: {
       y: direction === "up" ? 80 : direction === "down" ? -80 : 0,
