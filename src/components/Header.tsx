af406188d3b9e578b6af5e63425d36df5cac1189
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-01-25 03:06:40 +0300

    Refactor Header component and add accessibility attributes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 "use client"
+"use client"
 import Image from "next/image";
 import { Link } from "react-scroll";
 
-export default function Header (){
+export default function Header() {
   return (
     <header className="py-8 bg-fixed bg-no-repeat bg-cover bg-banner">
       <div className="container mx-auto">
@@ -11,14 +12,12 @@ export default function Header (){
             <Image width={50} height={50} src="/logo1.png" alt="logo" className="w-12 h-12" />
           </button>
           <div className="flex items-center justify-center">
-            <Link
-            to="contact"
-            activeClass="active"
-            smooth={true}
-            spy={true}
-            offset={50}
-            >
-              <button className="btn btn-sm">Let's Talk</button>
+            <Link to="contact"
+              aria-label="contact"
+              aria-description="link to contact section for contact form"
+              title="link to contact section for contact form"
+              smooth={true}>
+              <button className="btn btn-sm">Let&apos;s Talk</button>
             </Link>
           </div>
         </div>
