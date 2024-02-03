7bbe90292a68a75c36a29d52bc49d3c30e69385d
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-02-03 20:11:28 +0300

    Refactor page component and add background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,24 @@
-import Nav from '../components/Nav'
-import Header from "../components/Header";
+import Nav from '../components/Nav';
 import Banner from "../components/Banner";
 import Skills from "../components/skills/Skills";
 import Work from "../components/work/Work";
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
-
-
-export default function page() {
+import Image from 'next/image';
+import siteImg from "@/public/site-bg.webp";
+export default async function page() {
 
   return (
-    <div className="overflow-hidden bg-no-repeat bg-cover bg-site">
-      <Header />
+    <div className="relative">
+      <Image
+        src={siteImg}
+        alt='Background image'
+        fill
+        className='object-cover z-[-2]'
+        placeholder='blur'
+        priority={true}
+        sizes='100vw' />
       <Banner />
       <Skills />
       <Work />
