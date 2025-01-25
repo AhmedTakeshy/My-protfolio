fdd3cf85564f9de38bca78dfb0f8050199c5f381
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2025-01-25 15:45:47 +0300

    feat: Add Google site verification file and update robots.txt for improved SEO and bot management

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,12 +3,19 @@ import { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
 
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
-    sitemap: 'https://takeshy.vercel.app/sitemap.xml',
+    sitemap: ['https://takeshy.vercel.app/sitemap.xml', 'https://www.takeshy.vercel.app/sitemap.xml'],
     host: 'https://takeshy.vercel.app',
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/private/',
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+        disallow: "/nogooglebot/",
+      },
+    ],
   }
 }
