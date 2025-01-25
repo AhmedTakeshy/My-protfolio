37ff5c0eb4bc3b638d45d0bb917e17ae0bd81a5a
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2025-01-25 10:11:09 +0300

    refactor: Comment out unused GTM event import in Cookies component

--- src/components/cookies.tsx
+++ src/components/cookies.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { sendGTMEvent } from '@next/third-parties/google'
+// import { sendGTMEvent } from '@next/third-parties/google'
 import Link from 'next/link'
 import { useEffect, useState } from 'react';
 import { useGetCookie, useSetCookie } from 'cookies-next/client';
