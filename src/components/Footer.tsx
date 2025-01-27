4bae0dcf9a580de897ab584539ec4d3e9255b413
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2025-01-27 12:44:36 +0300

    fix: Update copyright year in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 
-export default function Footer (){
+export default function Footer() {
   return (
     <p className="text-white flex justify-center relative z-[51] sm:pb-[6px] text-base sm:text-lg shadow">
-      Copyright © {new Date().getFullYear()} Takeshy. All rights reserved.
+      Copyright © 2021 Takeshy. All rights reserved.
     </p>
   );
 };
