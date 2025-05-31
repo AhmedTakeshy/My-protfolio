84de3996a7b150a485c50344cb67d229bebc2f06
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2025-05-31 11:52:08 +0300

    chore: update Next.js configuration and remove package-lock.json
    - Simplified the optimizePackageImports setting in next.config.ts to include all react-icons.
    - Removed package-lock.json in favor of pnpm-lock.yaml for better dependency management.
    - Updated Next.js version in package.json to 15.4.0-canary.59.

--- next.config.ts
+++ next.config.ts
@@ -8,30 +8,7 @@ const bundleAnalyzer = withBundleAnalyzer({
 const nextConfig: NextConfig = {
   experimental: {
     ppr: "incremental",
-    optimizePackageImports: [
-      "react-icons/fa",
-      "react-icons/hi",
-      "react-icons/bs",
-      "react-icons/im",
-      "react-icons/bi",
-    ],
-  },
-  modularizeImports: {
-    "react-icons/fa": {
-      transform: "@react-icons/all-files/fa/{{ member }}",
-    },
-    "react-icons/hi": {
-      transform: "@react-icons/all-files/hi/{{ member }}",
-    },
-    "react-icons/bs": {
-      transform: "@react-icons/all-files/bs/{{ member }}",
-    },
-    "react-icons/im": {
-      transform: "@react-icons/all-files/im/{{ member }}",
-    },
-    "react-icons/ai": {
-      transform: "@react-icons/all-files/bi/{{ member }}",
-    },
+    optimizePackageImports: ["react-icons/*"],
   },
 };
 
